f0f89cc7bcc658b44d0d334116f0331a4c7ca91b
Billy Jacoby <billyjacoby@gmail.com>
2023-10-30 21:39:53 -0400

    chore: update about content

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Projects' },
 ]
 
 export default headerNavLinks
