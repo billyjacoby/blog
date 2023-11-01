2cd085aad6026053ec95dfc0dfb578a6a229ee0c
Billy Jacoby <billyjacoby@gmail.com>
2023-10-31 21:34:45 -0400

    chore: csp update again

--- next.config.js
+++ next.config.js
@@ -8,13 +8,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is *.googletagmanager.com *.google-analytics.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com 'strict-dynamic';
   frame-src giscus.app
 `;
 
